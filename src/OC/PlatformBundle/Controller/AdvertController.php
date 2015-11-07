e8c13f124544000438bf27e8d8c5e066dff8240c
DIJOUX Vincent <dijoux.vin@gmail.com>
2015-11-07 15:40:50 +0100

    Le routeur de symfony2

--- src/OC/PlatformBundle/Controller/AdvertController.php
+++ src/OC/PlatformBundle/Controller/AdvertController.php
@@ -9,6 +9,30 @@ class AdvertController extends Controller
 {
     public function indexAction()
     {
-        return $this->render('OCPlatformBundle:Advert:index.html.twig');
+        $url = $this->generateUrl('oc_platform_view', ['id' => 5 ]);
+
+        return new Response(sprintf(
+            "URL de l'annonce d'id 5 : %s",
+            $url
+        ));
+    }
+
+    public function viewAction($id)
+    {
+        return new Response(sprintf(
+            "Affichage de l'annonce d'id %d",
+            $id
+        ));
+    }
+
+    public function viewSlugAction($slug, $year, $_format)
+    {
+        return new Response(sprintf(
+            "On pourrait afficher l'annonce correspondant au slug
+            %s créée en %s et au format %s",
+            $slug,
+            $year,
+            $_format
+        ));
     }
 }
